5850c9536ad26770c8bda946e55cc18ed04428f2
wylliss <wyllissilva@gmail.com>
2020-11-02 12:41:19 -0300

    testando outra branch

--- flaskProject1/dashcw.py
+++ flaskProject1/dashcw.py
@@ -1,15 +1,28 @@
 #!/usr/bin/env python
+import json
+from pprint import pprint
+
 import boto3
 from datetime import datetime
 from datetime import timedelta
 import time
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output, State
+import yfinance as yf
+import plotly.graph_objects as go
+import pandas as pd
+from dash.exceptions import PreventUpdate
+import dash_table
+import plotly.express as px
 
 
 def initialize_client():
     client = boto3.client(
         'cloudwatch',
-        aws_access_key_id='AKIA5D4WV7YYGCSZZTOQ',
-        aws_secret_access_key='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',
+        aws_access_key_id='AKIA5D4WV7YYBVO3TD4E',
+        aws_secret_access_key='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',
         region_name='sa-east-1'
     )
 
@@ -73,19 +86,25 @@ def request_metricNO(client):
     return response_no
 
 
+
+
 def main():
     while True:
-        time.sleep(10),
+        time.sleep(5),
         client = initialize_client()
 
         response = request_metric(client)
         response_ni = request_metricNI(client)
         response_no = request_metricNO(client)
 
-        with open('static/saidadocw.json', 'w+') as arquivo:
-            [print(response, file=arquivo)]
-            [print(response_ni, file=arquivo)]
-            [print(response_no, file=arquivo)]
+        pf = pd.DataFrame(response['Datapoints'])
+        print(pf)
+
+        pfni = pd.DataFrame(response_ni['Datapoints'])
+        print(pfni)
+
+        pfno = pd.DataFrame(response_no['Datapoints'])
+        print(pfno)
 
 
 main()
