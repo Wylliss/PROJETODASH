2a0032381681d99530cc84d033215cd56d08c1f9
wylliss <wyllissilva@gmail.com>
2020-11-02 17:32:23 -0300

    Primeiros resultados do front ok

--- flaskProject1/dashcw.py
+++ flaskProject1/dashcw.py
@@ -1,32 +1,22 @@
-#!/usr/bin/env python
-import json
-from pprint import pprint
-
+import time
+import dash
+import dash_html_components as html
+from dash.dependencies import Input, Output, State
 import boto3
 from datetime import datetime
 from datetime import timedelta
-import time
-import dash
-import dash_core_components as dcc
-import dash_html_components as html
-from dash.dependencies import Input, Output, State
-import yfinance as yf
-import plotly.graph_objects as go
 import pandas as pd
-from dash.exceptions import PreventUpdate
-import dash_table
-import plotly.express as px
 
 
 def initialize_client():
-    client = boto3.client(
-        'cloudwatch',
-        aws_access_key_id='AKIA5D4WV7YYBVO3TD4E',
-        aws_secret_access_key='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',
-        region_name='sa-east-1'
-    )
+        client = boto3.client(
+            'cloudwatch',
+            aws_access_key_id='AKIA5D4WV7YYBVO3TD4E',
+            aws_secret_access_key='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',
+            region_name='sa-east-1'
+        )
 
-    return client
+        return client
 
 
 def request_metric(client):
@@ -85,26 +75,53 @@ def request_metricNO(client):
 
     return response_no
 
+app = dash.Dash()
+app.layout = html.Div(
+    [
+        html.Button("PC WYLLIS", id="button_3"),
+        html.Button("PC MARCUS", id="button_4"),
+        html.Div(children="CPU", id="CPU"),
+        html.Div(children="NET IN", id="NET IN"),
+        html.Div(children="NET OUT", id="NET OUT"),
+    ]
+)
 
 
-
-def main():
+@app.callback(
+    Output("CPU", "children"),
+    [Input("button_3", "n_clicks")])
+def first_callback(a):
     while True:
-        time.sleep(5),
+        time.sleep( 5 )
         client = initialize_client()
-
-        response = request_metric(client)
-        response_ni = request_metricNI(client)
-        response_no = request_metricNO(client)
-
-        pf = pd.DataFrame(response['Datapoints'])
-        print(pf)
-
-        pfni = pd.DataFrame(response_ni['Datapoints'])
-        print(pfni)
-
-        pfno = pd.DataFrame(response_no['Datapoints'])
-        print(pfno)
+        response = request_metric( client )
+        dfa = pd.DataFrame( response['Datapoints'] )
+        return "MÉDIA CPU", dfa["Average"].values[0], " ", dfa["Unit"].values[0]
 
 
-main()
+@app.callback(
+    Output( "NET IN", "children" ),
+    [Input( "button_3", "n_clicks" )] )
+def second_callback(b):
+    while True:
+        time.sleep( 5 )
+        client = initialize_client()
+        response_ni = request_metricNI( client )
+        dfb = pd.DataFrame(response_ni['Datapoints'])
+        return "MÉDIA INTERNET IN",dfb["Average"].values[0], " ", dfb["Unit"].values[0]
+
+
+@app.callback(
+    Output( "NET OUT", "children" ),
+    [Input( "button_3", "n_clicks" )] )
+def third_callback(c):
+    while True:
+        time.sleep( 5 )
+        client = initialize_client()
+        response_no= request_metricNO(client)
+        df = pd.DataFrame( response_no['Datapoints'] )
+        return "MÉDIA INTERNET OUT", df["Average"].values[0], " ", df["Unit"].values[0]
+
+
+if __name__ == '__main__':
+    app.run_server( debug=True, port=8055 )
